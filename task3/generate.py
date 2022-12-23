05dcc1f33beb769b8b82472de280606e63e45bc7
Or Levitas <orlevitas@gmail.com>
2022-12-23 15:41:26 +0200

    init

---
+++ task3/generate.py
@@ -0,0 +1,64 @@
+import sys
+import time
+import random
+import argparse
+from collections import defaultdict
+
+class PCFG(object):
+    def __init__(self):
+        self._rules = defaultdict(list)
+        self._sums = defaultdict(float)
+
+    def add_rule(self, lhs, rhs, weight):
+        assert(isinstance(lhs, str))
+        assert(isinstance(rhs, list))
+        self._rules[lhs].append((rhs, weight))
+        self._sums[lhs] += weight
+
+    @classmethod
+    def from_file(cls, filename):
+        grammar = PCFG()
+        with open(filename) as fh:
+            for line in fh:
+                line = line.split("#")[0].strip()
+                if not line: continue
+                w,l,r = line.split(None, 2)
+                r = r.split()
+                w = float(w)
+                grammar.add_rule(l,r,w)
+        return grammar
+
+    def is_terminal(self, symbol): return symbol not in self._rules
+
+    def gen(self, symbol):
+        if self.is_terminal(symbol): return symbol
+        else:
+            expansion = self.random_expansion(symbol)
+            return " ".join(self.gen(s) for s in expansion)
+
+    def random_sent(self):
+        return self.gen("ROOT")
+
+    def random_expansion(self, symbol):
+        """
+        Generates a random RHS for symbol, in proportion to the weights.
+        """
+        p = random.random() * self._sums[symbol]
+        for r,w in self._rules[symbol]:
+            p = p - w
+            if p < 0: return r
+        return r
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--number', type=str, default=1, help='Number of sentences')
+    args, unknown = parser.parse_known_args()
+ 
+    return (args,unknown)
+
+if __name__ == '__main__':
+    args, f_name = parse_arguments()
+    pcfg = PCFG.from_file(f_name[0])
+
+    for i in range(int(args.number)):
+        print(f'{i + 1}) {pcfg.random_sent()}')
